4d034d26d7c873c1e05e2df8cec4002408fecad0
David Ding <dingtechxing16@gmail.com>
2023-06-28 10:52:30 +0100

    Moved images folder into src and image rendering succesfully

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "../styles/navbar.css";
+import houseLogo from "../images/house.png";
 
 const NavBar = () => {
   return (
     <nav className="nav-bar">
       <div>
-        <img src="../../images/town.png" alt="house-logo" />
+        <img src={houseLogo} alt="house-logo" />
         <ul className="navbar-links">
           <li className="navbar-links-item">View Properties</li>
           <li className="navbar-links-item">Add a Property</li>
